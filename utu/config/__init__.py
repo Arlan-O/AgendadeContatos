3d761abbf93aaa77365159c69bdcbc15c2d3eba3
Arlan <arlan.oliveira022@gmail.com>
2025-08-30 20:08:18 -0300

    Delete utu directory

--- utu/config/__init__.py
+++
@@ -1,6 +0,0 @@
-from .agent_config import AgentConfig, ToolkitConfig
-from .eval_config import EvalConfig
-from .loader import ConfigLoader
-from .model_config import ModelConfigs, ModelSettingsConfig
-
-__all__ = ["ConfigLoader", "AgentConfig", "ToolkitConfig", "EvalConfig", "ModelConfigs", "ModelSettingsConfig"]
--- utu/eval/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from ...db import EvaluationSample
-from .data_manager import BaseDataManager, DBDataManager, FileDataManager
-
-__all__ = ["EvaluationSample", "FileDataManager", "DBDataManager", "BaseDataManager"]
