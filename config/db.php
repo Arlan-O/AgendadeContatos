344e7addc3b02ee0fdcac14ed0e942062874842c
Arlan <arlan.oliveira022@gmail.com>
2025-09-12 11:15:09 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -9,9 +9,9 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$dbName;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Erros como exceções
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Retorno padrão como array associativo
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // Usa prepares nativos do MySQL
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+    PDO::ATTR_EMULATE_PREPARES   => false,                  
 ];
 
 try {
